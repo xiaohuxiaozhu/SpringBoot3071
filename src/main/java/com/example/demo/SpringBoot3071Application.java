3b14e2e27944828ff4494f56f4d00e010f8e6303
togoboy8 <cedrichillah@gmail.com>
2019-07-17 16:22:29 -0400

    first commit

---
+++ src/main/java/com/example/demo/SpringBoot3071Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3071Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3071Application.class, args);
+	}
+
+}
